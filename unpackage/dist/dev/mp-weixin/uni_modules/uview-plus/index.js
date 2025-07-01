752e654af7dbfbb4b2c0377613ad1df2cd0c72a5
houruijun.0317 <houruijun.0317@bytedance.com>
2025-07-01 11:12:20 +0800

    chore: 忽略unpackage临时产物

--- unpackage/dist/dev/mp-weixin/uni_modules/uview-plus/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-require("../../common/vendor.js");
-require("./libs/function/index.js");
-require("./libs/util/route.js");
-require("./libs/config/config.js");
-require("./libs/config/props.js");
-require("./libs/function/http.js");
-//# sourceMappingURL=../../../.sourcemap/mp-weixin/uni_modules/uview-plus/index.js.map
--- unpackage/dist/dev/mp-weixin/uni_modules/uview-plus/libs/function/calc.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=../../../../../.sourcemap/mp-weixin/uni_modules/uview-plus/libs/function/calc.js.map
