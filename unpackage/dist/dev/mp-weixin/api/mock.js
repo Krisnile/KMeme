2a9bc686c93eebcf528eb8ae59c2514a2ded8d7c
Krisnile <J040302YJ@163.com>
2025-07-01 11:42:39 +0800

    Remove node_modules and unpackage from repository and add to .gitignore

--- unpackage/dist/dev/mp-weixin/api/mock.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const common_vendor = require("../common/vendor.js");
-{
-  common_vendor.index.__f__("log", "at api/mock.js:64", "[mock] 非 H5 开发环境，mock 服务未启用");
-}
-//# sourceMappingURL=../../.sourcemap/mp-weixin/api/mock.js.map
--- unpackage/dist/dev/mp-weixin/uni_modules/uview-plus/libs/function/platform.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=../../../../../.sourcemap/mp-weixin/uni_modules/uview-plus/libs/function/platform.js.map
